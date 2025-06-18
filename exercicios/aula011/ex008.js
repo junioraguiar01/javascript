f9bbe27ea2a3e181704f84a546af71c367ad9d97
junioraguiar01 <junioraguiar2023@gmail.com>
2025-06-18 02:54:56 -0300

    aula 11 completa

---
+++ exercicios/aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 54
+console.log('A velocidade do seu carro e de ' +vel+'km/h')
+if(vel > 60){
+   console.log('você ultrapasou a velocidade da via e foi multado')
+}
+console.log('Não esqueça o cinto de segurança')
---
+++ exercicios/aula011/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'italia'
+console.log('voçé é do(a) '+pais)
+if(pais == 'Brasil'){
+    console.log('Então voçê é Brasileiro')
+}
+else{
+    console.log('Então voçê é Gringo')
+}
