d7d9ad7e00c00ce8d29418a5af5b6cc9d0a46e16
junioraguiar01 <junioraguiar2023@gmail.com>
2025-06-20 13:36:22 -0300

    exercicio atualizado

--- exercicios/aula012ex/ex015/script.js
+++ exercicios/aula012ex/ex015/script.js
@@ -10,12 +10,42 @@ function verificar(){
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             gênero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'imagens/foto-bebe-m.png')
+            }
+           else if (idade < 21){
+                    //jovem
+                    img.setAttribute('src', 'imagens/homem.png')
+            }
+            else if (idade < 50) {
+                //
+                img.setAttribute('src', 'imagens/homem.png')
+            }
+            else {
+                //idoso
+                img.setAttribute('src', 'imagens/homem.png')
+            }
         }else if (fsex[1].checked) {
             gênero = 'Mulher'
+              if (idade >=0 && idade < 10) {
+                //criança
+            }
+                else if (idade < 21){
+                    //jovem
+            }
+            else if (idade < 50) {
+                //Adulto
+            }
+            else {
+                //idoso
+            }
         }
-       
+        res.style.textAlign = 'center'
         res.innerHTML = 'Você tem '+idade+ ' Anos. você é '+gênero
     }
 }
