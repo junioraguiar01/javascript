dca6cd11e35ec8df458cd2a979af8533ca0327a5
junioraguiar01 <junioraguiar2023@gmail.com>
2025-06-20 03:12:05 -0300

    exercicio atualizado

---
+++ exercicios/aula012ex/ex015/script.js
@@ -0,0 +1,21 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    
+    if (fano.value.length == 0 || Number(fano.value) > ano){
+        window.alert('[ERRO] Verifique os Dados')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+        }else if (fsex[1].checked) {
+            gênero = 'Mulher'
+        }
+       
+        res.innerHTML = 'Você tem '+idade+ ' Anos. você é '+gênero
+    }
+}
